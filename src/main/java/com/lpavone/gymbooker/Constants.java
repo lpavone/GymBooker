308b4bd5771ec2ec09313692d89ac6eb1393ec85
Leonardo Pavone <leonardopavone@gmail.com>
2017-09-06 23:37:22 +0100

    added back masked login details

---
+++ src/main/java/com/lpavone/gymbooker/Constants.java
@@ -0,0 +1,17 @@
+package com.lpavone.gymbooker;
+
+/**
+ * Created by leonardo on 19/03/17.
+ */
+class Constants {
+    public static final String TIMETABLE_PAGE = "https://swanleisure.legendonlineservices.co.uk/enterprise/BookingsCentre/MemberTimetable";
+    public static final String WEBSITE_HOME_URL = "https://swanleisure.legendonlineservices.co.uk/enterprise/account/login";
+    public static final String CONFIRM_LINK = "https://swanleisure.legendonlineservices.co.uk/enterprise/Basket/Pay";
+    //LOGIN ELEMENTS
+    public static final String EMAIL_FIELD_NAME = "login.Email";
+    public static final String PASSWORD_FIELD_NAME = "login.Password";
+    public static final String LEO_LOGIN = "xxxxxxxxxxxxil.com,xxxx";
+    public static final String CLAU_LOGIN = "xxxxxxxxxxxxail.com,xxxx";
+    public static final String FILENAME = "_BookedClasses.csv";
+    public static final String LOGOUT = "https://swanleisure.legendonlineservices.co.uk/enterprise/Account/Logout";
+}
